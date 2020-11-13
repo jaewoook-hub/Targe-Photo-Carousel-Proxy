d2693f2f347c94e33ae56112d86bce4a180cc623
Richard Ok <jaeok2017@u.northwestern.edu>
2020-11-12 23:50:18 -0600

    Re-commiting and pushing my work on my personal repo

---
+++ README.md
@@ -0,0 +1,11 @@
+# turner_proxy
+Proxy server for Front End Capstone Project
+
+Team 2 - Brucequalis
+
+Modeled after a Target product page web app. Individual Services are:
+- Image Carousel (Michael Tobias)
+- Product Options (Aziz Bouland)
+- Recommended Products (Turner Kraus)
+- Reviews (Jason Strunk)
+
---
+++ index.js
@@ -0,0 +1,21 @@
+const newRelic = require('newrelic');
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+const HOST = 'localhost';
+
+app.use(express.static(__dirname));
+
+app.get('/info', (req, res, next) => {
+  res.send('This is a proxy service which proxies to the 4 services present in the Team 2 FEC project.');
+});
+
+app.get('/*', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
